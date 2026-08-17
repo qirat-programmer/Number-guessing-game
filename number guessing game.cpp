24ac33b471a39037784135712bfab86674155e22
Qirat Iqbal <qiratiqbal987@gmail.com>
2026-08-17 04:30:13 -0700

    Add files via upload

---
+++ number guessing game.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+
+    srand(time(0));
+    number = rand() % 100 + 1;
+
+    printf("=================================\n");
+    printf("      NUMBER GUESSING GAME\n");
+    printf("=================================\n");
+    printf("Guess a number between 1 and 100\n\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess > number) {
+            printf("Too high! Try again.\n");
+        }
+        else if (guess < number) {
+            printf("Too low! Try again.\n");
+        }
+        else {
+            printf("\nCongratulations! You guessed it! ??\n");
+            printf("Number of attempts: %d\n", attempts);
+        }
+
+    } while (guess != number);
+
+    return 0;
+}
